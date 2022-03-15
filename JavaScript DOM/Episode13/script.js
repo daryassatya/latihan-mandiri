a52aa63518676e4fb7bdb0779f8f8b30e0725bd1
Daryassatya <daryassatya@gmail.com>
2022-03-15 22:11:39 +0700

    JavaScript DOM Episode 13

---
+++ JavaScript DOM/Episode13/script.js
@@ -0,0 +1,27 @@
+
+// -- Cara Kurang Baik --
+// const jumboImg = document.querySelector('.jumbo');
+// const thumbImg = document.querySelectorAll('.thumb');
+
+// thumbImg.forEach(function(element){
+//     element.addEventListener('click', function(event){
+//         jumboImg.classList.remove('fade');
+//         const getAttr = element.getAttribute('src');
+//         jumboImg.setAttribute('src', getAttr);
+//         jumboImg.classList.add('fade');
+//     })
+// });
+
+const container = document.querySelector('.container');
+const jumbo = document.querySelector('.jumbo');
+
+container.addEventListener('click', function(event){
+    if(event.target.className == 'thumb'){
+        jumbo.src = event.target.src;
+        jumbo.classList.add('fade');
+        setTimeout(function(){
+            jumbo.classList.remove('fade');
+        }, 500); //Bisa dipercepat namun animasinya tidak akan sampai 500 milisecond
+    }
+})
+
