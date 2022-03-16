674985c1af7fccdd30a5fb017e9afe89a96af59a
Daryassatya <daryassatya@gmail.com>
2022-03-16 17:05:25 +0700

    Adding new Folder Python

---
+++ Python/Python Dasar/Episode3/src/mian.py
@@ -0,0 +1,18 @@
+# print("Hello World!!!")
+# print("Apakabar")
+
+# a = 10
+a = int(input("Masukkan Awal : "))
+b = int(input("Masukkan Akhir : "))
+
+x = []
+y = []
+
+for i in range(x,y):
+    if a%i == 0 :
+        i.append(i)
+    if b%i == 1 :
+        i.append(i)
+
+print(x)
+print(y)
