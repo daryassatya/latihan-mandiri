948cb6f2a5f140c5621e473b8a7f84a923343641
Daryassatya <daryassatya@gmail.com>
2022-03-17 18:23:46 +0700

    JavaScript Lanjutan - Ulasan Objek Dasar

--- JavaScript/JavaScriptLanjutan/Episode1/script.js
+++ JavaScript/JavaScriptLanjutan/Episode1/script.js
@@ -14,18 +14,42 @@
 // }
 
 //  2.  Function Declaration
-function Mahasiswa(nama, energi){
-    let mahasiswa = {};
-    mahasiswa.nama = nama;
-    mahasiswa.energi = energi;
+// function Mahasiswa(nama, energi){
+//     let mahasiswa = {};
+//     mahasiswa.nama = nama;
+//     mahasiswa.energi = energi;
 
-    mahasiswa.makan = function(porsi){
-        this.energi += porsi;
-        console.log(`Halo, ${this.nama} Selamat  Makan `); //string khusus untuk langsung menconcate
-    }
+//     mahasiswa.makan = function(porsi){
+//         this.energi += porsi;
+//         console.log(`Halo, ${this.nama} Selamat  Makan! `); //string khusus untuk langsung menconcate
+//     }
 
-    return mahasiswa;
-}
+//     mahasiswa.main = function(jam){
+//         this.energi -= jam;
+//         console.log(`Halo, ${this.nama} Selamat  Bermain! `); //string khusus untuk langsung menconcate
+//     }
+
+//     return mahasiswa;
+// }
+
+// let panggilObject1 = Mahasiswa('Dimas Aryasatya', 10); 
+//  tidak bisa langsung memasukkan value kedalam parameter yang berisi objek function
 
 //  3.  Constructor function
+function Mahasiswa(nama, energi){
+    this.nama = nama;
+    this.energi = energi;
+
+    this.makan = function(porsi){
+        this.energi += porsi;
+        console.log(`Halo, ${this.nama} Selamat  Makan! `); //string khusus untuk langsung menconcate
+    }
+
+    this.main = function(jam){
+        this.energi -= jam;
+        console.log(`Halo, ${this.nama} Selamat  Bermain! `); //string khusus untuk langsung menconcate
+    }
+}
+let panggilObject1 = new Mahasiswa('Dimas Aryasatya', 10);
+
 //  4.  Object Create
