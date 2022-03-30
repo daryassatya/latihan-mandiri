2918f701274bdbf2c00da3132df88d11520c3749
Daryassatya <daryassatya@gmail.com>
2022-03-30 17:29:40 +0700

    JavaScript Lanjutan Episode18

---
+++ JavaScript/JavaScriptLanjutan/Episode18/script.js
@@ -0,0 +1,57 @@
+//  Rest Parameter
+// "Mempresentasikan argument pada function dengan jumlah tidak terbatas menjadi sebuah array"
+
+// Test 1
+// harus ditaruh diakhir atau ditaruh sendiri
+function myFunc(a,b, ...myArgs /*Dapat mengambil semua atau sisa yg dikirim parameter*/){
+    return `a = ${a}, b= ${b}, myArgs = ${myArgs}`;
+    // return arguments; //yang di return bukanlah sebuah array, namun objek
+
+    // cara mengubah objek jadi array :
+    // return Array.from(arguments);
+    // return [...arguments];
+}
+console.log(myFunc(1,2,3,4,5));
+
+
+// Test 2 (Menjumlah)
+function jumlahkan(...angka){
+
+    // --- Cara 1 ---
+    // let total = 0;
+    //     for(const a of angka){
+    //         total+=a;
+    //     }
+    // return total;
+
+    // --- Cara 2 ---
+    return angka.reduce((currentVal, loop) => currentVal+loop);
+}
+console.log(jumlahkan(1,2,3,4,5));
+
+
+// Test 3 (Array Destructuring)
+const kelompok1 = ['Dimas', 'Arya', 'Satya', 'Ganteng', 'Banget'];
+const [ketua, wakil, ...anggota] = kelompok1;
+console.log(ketua, wakil, anggota); // variabel anggota akan menjadi array
+
+
+// Test 4 (Object Destructuring)
+const team = {
+    pm: 'Dimas',
+    frontEnd1: 'Arya',
+    frontEnd2: 'Satya',
+    backEnd: 'Daryas',
+    ux: 'Kumbang',
+    devOps: 'Orang'
+}
+const {pm, ...myTeam} = team;
+console.log(pm);
+
+
+// Test 5 (Filtering)
+function filterBy(type, ...values){
+    return values.filter(val => typeof val === type /* atau 'number' */);
+    // return [type, filter];
+}
+console.log(filterBy('number', 1,2,'Dimas', false, 10, true, 'Arya'))
